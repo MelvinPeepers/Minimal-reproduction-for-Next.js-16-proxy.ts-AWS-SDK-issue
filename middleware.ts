ed6a404e8b4b1bba8be7dba15a2a27064841ccd8
Melvin Peepers <melvinpeepers@Mac.lan>
2025-11-20 05:58:55 -0600

    Test AWS SDK behavior using middleware.ts instead of proxy.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider';
 import { NextResponse } from 'next/server';
 
-export async function proxy() {
+export async function middleware() {
   new CognitoIdentityProviderClient({ region: 'us-east-1' });
   return NextResponse.next();
 }
